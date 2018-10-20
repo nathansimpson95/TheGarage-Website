2c698835693a934806a34c55341c65dc60714232
Nathan Simpson <hello@nathansimpson.design>
2018-10-20 14:45:51 +1100

    [WIP] Creating Flexbox layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,77 @@
 import React from 'react'
-import { Link } from 'gatsby'
+//import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+//import Layout from '../components/layout'
 
 import Hero from '../components/hero'
 import Form from '../components/form'
 import Footer from '../components/footer'
 
-// content = [
-//   {
-//     heading: 'For All of Sydney’s Car Enthusiasts',
-//     content: '',
-//     image: '',
-//     side: 'left',
-//   },{
-//     heading: 'Work on cars with others',
-//     content: '',
-//     image: '',
-//     side: 'right',
-//   },{
-//     heading: 'Showcase your Ride',
-//     content: '',
-//     image: '',
-//     side: 'left',
-//   },{
-//     heading: 'More than a Social Network',
-//     content: '',
-//     image: '',
-//     side: 'right',
-//   },
-// ]
+const content = [
+  {
+    heading: 'For All of Sydney’s Car Enthusiasts',
+    content: 'hi',
+    black: true,
+    image: 'http://www.fourfingerdiscount.com.au/wp-content/uploads/2016/02/moleman-cowabunga.jpg',
+    side: 'left',
+  },{
+    heading: 'Showcase your ride',
+    content: 'hi',
+    image: 'http://www.fourfingerdiscount.com.au/wp-content/uploads/2016/02/moleman-cowabunga.jpg',
+    side: 'right',
+  },{
+    heading: 'Work on cars with others',
+    content: 'hi',
+    black: true,
+    image: 'http://www.fourfingerdiscount.com.au/wp-content/uploads/2016/02/moleman-cowabunga.jpg',
+    side: 'left',
+  },{
+    heading: 'More than a Social Network',
+    content: 'hi',
+    image: 'http://www.fourfingerdiscount.com.au/wp-content/uploads/2016/02/moleman-cowabunga.jpg',
+    side: 'right',
+  },
+]
 
-// const contentRow = (heading, content, image, side) => (
-//   <section>
-//     {side == 'right' ? <img src={image} /> : null}
-//     <div>
-//       <h2>heading</h2>
-//       <p>content</p>
-//     </div>
-//     {side == 'right' ? <img src={image} /> : null}
-//   </section>
-// )
+const ContentRow = block => (
+  <section
+    style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        background: block.data.black ? '#080808' : 'none'
+      }}
+  >
+    <div style={{
+      flex: 1,
+      display: 'flex',
+      padding:20,
+      boxSizing: 'content-box',
+      alignItems: 'center'
+    }}>
+      <div>
+        <h2>{block.data.heading}</h2>
+        <p>{block.data.content}</p>
+      </div>
+    </div>
+
+    <div style={{
+      flex: 1,
+      padding: 0
+    }}>
+      <img style={{margin: 0, padding: 0}} src={block.data.image}/>
+    </div>
+  </section>
+)
 
 const IndexPage = () => (
   <div>
     <Hero />
+    <div>
+      {content.map(data => 
+        <ContentRow data={data}/>
+      )}
+    </div>
     <Form />
     <Footer />
   </div>
